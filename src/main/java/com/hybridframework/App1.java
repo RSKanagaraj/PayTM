f8d4f96c4077a48b0f4c674a973e7083dbd681d4
Kanagaraj <kanagaraj.soundararajan@efmfm.com>
2017-12-15 09:14:50 +0530

    test2

---
+++ src/main/java/com/hybridframework/App1.java
@@ -0,0 +1,18 @@
+package com.hybridframework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class App1{
+	@Test
+	public void abc()
+	{
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "./driversexe/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());
+	}
+	
+}
