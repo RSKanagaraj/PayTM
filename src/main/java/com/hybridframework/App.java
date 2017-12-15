a80c55cde03a7c1ff10d3ac58aca3006c7edc973
Kanagaraj <kanagaraj.soundararajan@efmfm.com>
2017-12-15 08:50:10 +0530

    test
    git commit -m "first commit"

---
+++ src/main/java/com/hybridframework/App.java
@@ -0,0 +1,19 @@
+package com.hybridframework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class App {
+
+	@Test
+	public void abc()
+	{
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "./driversexe/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());
+	}
+
+}
